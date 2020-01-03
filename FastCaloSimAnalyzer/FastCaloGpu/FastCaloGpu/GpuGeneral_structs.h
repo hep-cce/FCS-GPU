af58733d6a61bf0b5da0039e32d1f0191d44384d
Zihua Dong <zdong@bnl.gov>
2020-01-03 16:05:07 -0500

    adding mssing file

---
+++ FastCaloSimAnalyzer/FastCaloGpu/FastCaloGpu/GpuGeneral_structs.h
@@ -0,0 +1,10 @@
+#ifndef GPU_GENERAL_STRUCT_
+#define GPU_GENERAL_STRUCT_H  
+                             
+#include "FH_structs.h"     
+typedef struct Cell_E {    
+  unsigned long cellid ;  
+  float energy ;         
+} Cell_E ;              
+                       
+#endif
