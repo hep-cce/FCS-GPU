6c3f88d09c84618a29d8ea3a5dc0dae1d541a07b
Mohammad Atif <mohammadatif4461@gmail.com>
2022-07-06 09:55:27 -0400

    added GeoLg and regionn data copy in tests

--- FastCaloSimAnalyzer/macro/runTFCSTests.cxx
+++ FastCaloSimAnalyzer/macro/runTFCSTests.cxx
@@ -7,8 +7,8 @@
 #include "../FastCaloGpu/FastCaloGpu/Rand4Hits.h"
 #include "../FastCaloGpu/FastCaloGpu/GeoLoadGpu.h"
 
-//#include "TFCSSampleDiscovery.h"
-#include "CaloGeometryFromFile.h"
+#include "../FastCaloSimAnalyzer/CaloGeometryFromFile.h"
+#include "../FastCaloSimCommon/src/TFCSSampleDiscovery.h"
 
 #define MAXHITS 200000
 #define MAXBINS 1024
@@ -111,6 +111,101 @@ TEST_CASE( "Compare stats of RNs generated on CPU and GPU" ) {
   REQUIRE( std::fabs( cpu_moments[1] - gpu_moments[1] ) < epsilon );
 }
 
+
+
+
+inline void region_data_cpy( CaloGeometryLookup* glkup, GeoRegion* gr ) {
+
+  // Copy all parameters
+  gr->set_xy_grid_adjustment_factor( glkup->xy_grid_adjustment_factor() );
+  gr->set_index( glkup->index() );
+
+  int neta = glkup->cell_grid_eta();
+  int nphi = glkup->cell_grid_phi();
+  // std::cout << " copy region " << glkup->index() << "neta= " << neta<< ", nphi= "<<nphi<< std::endl ;
+
+  gr->set_cell_grid_eta( neta );
+  gr->set_cell_grid_phi( nphi );
+
+  gr->set_mineta( glkup->mineta() );
+  gr->set_minphi( glkup->minphi() );
+  gr->set_maxeta( glkup->maxeta() );
+  gr->set_maxphi( glkup->maxphi() );
+
+  gr->set_mineta_raw( glkup->mineta_raw() );
+  gr->set_minphi_raw( glkup->minphi_raw() );
+  gr->set_maxeta_raw( glkup->maxeta_raw() );
+  gr->set_maxphi_raw( glkup->maxphi_raw() );
+
+  gr->set_mineta_correction( glkup->mineta_correction() );
+  gr->set_minphi_correction( glkup->minphi_correction() );
+  gr->set_maxeta_correction( glkup->maxeta_correction() );
+  gr->set_maxphi_correction( glkup->maxphi_correction() );
+
+  gr->set_eta_correction( glkup->eta_correction() );
+  gr->set_phi_correction( glkup->phi_correction() );
+  gr->set_deta( glkup->deta() );
+  gr->set_dphi( glkup->dphi() );
+
+  gr->set_deta_double( glkup->deta_double() );
+  gr->set_dphi_double( glkup->dphi_double() );
+
+  // now cell array copy from GeoLookup Object
+  // new cell_grid is a unsigned long array
+  long long* cells = (long long*)malloc( sizeof( long long ) * neta * nphi );
+  gr->set_cell_grid( cells );
+
+  if ( neta != (int)( *( glkup->cell_grid() ) ).size() )
+    std::cout << "neta " << neta << ", vector eta size " << ( *( glkup->cell_grid() ) ).size() << std::endl;
+  for ( int ie = 0; ie < neta; ++ie ) {
+    //    	if(nphi != (*(glkup->cell_grid()))[ie].size() )
+    //		 std::cout<<"neta " << neta << "nphi "<<nphi <<", vector phi size "<<  (*(glkup->cell_grid()))[ie].size()
+    //<< std::endl;
+
+    for ( int ip = 0; ip < nphi; ++ip ) {
+
+      //	if(glkup->index()==0 ) std::cout<<"in loop.."<< ie << " " <<ip << std::endl;
+      auto c = ( *( glkup->cell_grid() ) )[ie][ip];
+      if ( c ) {
+        cells[ie * nphi + ip] = c->calo_hash();
+
+      } else {
+        cells[ie * nphi + ip] = -1;
+        //	        std::cout<<"NUll cell in loop.."<< ie << " " <<ip << std::endl;
+      }
+    }
+  }
+}
+
+inline void GeoLg( CaloGeometryFromFile* m_geo, GeoLoadGpu* m_gl ) {
+  m_gl = new GeoLoadGpu();
+  m_gl->set_ncells( m_geo->get_cells()->size() );
+  m_gl->set_max_sample( CaloGeometry::MAX_SAMPLING );
+  int nrgns = m_geo->get_tot_regions();
+
+  std::cout << "Total GeoRegions= " << nrgns << std::endl;
+  std::cout << "Total cells= " << m_geo->get_cells()->size() << std::endl;
+
+  m_gl->set_nregions( nrgns );
+  m_gl->set_cellmap( m_geo->get_cells() );
+
+  GeoRegion* GR_ptr = (GeoRegion*)malloc( nrgns * sizeof( GeoRegion ) );
+  m_gl->set_regions( GR_ptr );
+
+  Rg_Sample_Index* si = (Rg_Sample_Index*)malloc( CaloGeometry::MAX_SAMPLING * sizeof( Rg_Sample_Index ) );
+
+  m_gl->set_sample_index_h( si );
+
+  int i = 0;
+  for ( int is = 0; is < CaloGeometry::MAX_SAMPLING; ++is ) {
+    si[is].index = i;
+    int nr       = m_geo->get_n_regions( is );
+    si[is].size  = nr;
+    for ( int ir = 0; ir < nr; ++ir ) region_data_cpy( m_geo->get_region( is, ir ), &GR_ptr[i++] );
+    //    std::cout<<"Sample " << is << "regions: "<< nr << ", Region Index " << i << std::endl ;
+  }
+}
+
 /* In this test, Geometry parameters from GPU are copied to CPU and
  * compared with the original on CPU.
  */
@@ -118,16 +213,24 @@ TEST_CASE( "Compare Geometry Params" ) {
 
   CaloGeometryFromFile* m_geo = new CaloGeometryFromFile();
 
-  std::string GeometryPath = "/work/atif/FastCaloSimInputs/CaloGeometry/";
+  std::string GeometryPath = "/work/atif/FastCaloSimInputs";
 
   // load geometry files
-  //m_geo->LoadGeometryFromFile( "/work/atif/FastCaloSimInputs/CaloGeometry/Geometry-ATLAS-R2-2016-01-00-01.root",
-        //                     "ATLAS-R2-2016-01-00-01",
-          //                     "/work/atif/FastCaloSimInputs/CaloGeometry/cellId_vs_cellHashId_map.txt" );
+  m_geo->LoadGeometryFromFile( GeometryPath + "/CaloGeometry/Geometry-ATLAS-R2-2016-01-00-01.root",
+                               "ATLAS-R2-2016-01-00-01",
+                               GeometryPath + "/CaloGeometry/cellId_vs_cellHashId_map.txt" );
 
-  //m_geo->LoadFCalGeometryFromFiles( TFCSSampleDiscovery::geometryNameFCal() );
+  std::string geoPathFCal1 = GeometryPath + "/CaloGeometry/FCal1-electrodes.sorted.HV.09Nov2007.dat";
+  std::string geoPathFCal2 = GeometryPath + "/CaloGeometry/FCal2-electrodes.sorted.HV.April2011.dat";
+  std::string geoPathFCal3 = GeometryPath + "/CaloGeometry/FCal3-electrodes.sorted.HV.09Nov2007.dat";
+  m_geo->LoadFCalGeometryFromFiles( TFCSSampleDiscovery::geometryNameFCal() );
 
-  std::cout << "------ m_geo->get_cells()->size() = " << m_geo->get_cells()->size() << std::endl;
+  //std::cout << "------ m_geo->get_cells()->size() = " << m_geo->get_cells()->size() << std::endl;
+  
+  GeoLoadGpu* m_gl;
+  GeoLg ( m_geo, m_gl );
+  m_gl->LoadGpu();
+
 //  /********** Test ********/
 //  GeoRegion* m_regions_host{0};
 //  m_regions_host = (GeoRegion *) malloc( sizeof( GeoRegion ) * m_nregions );
