fde34cdc5b11604b5abd0b8bddbf5282873e1a68
leggett <leggett@cern.ch>
2022-12-03 11:25:06 -0800

    use template for CU_BigMem to simplify usage

--- FastCaloSimAnalyzer/FastCaloGpu/FastCaloGpu/CU_BigMem.h
+++ FastCaloSimAnalyzer/FastCaloGpu/FastCaloGpu/CU_BigMem.h
@@ -1,64 +1,66 @@
 #ifndef CU_BIGMEM_H
 #define CU_BIGMEM_H
 
-
 #include "gpuQ.h"
 #include <vector>
 
-class CU_BigMem  {
+class CU_BigMem {
 
-public :
-	CU_BigMem(): m_seg_size(0), m_ptrs(0), m_used(0), m_seg(0)   { } ; 
-	CU_BigMem( size_t s ) {  //initialize to one seg with size s
-	  void* p ;  
-	  m_seg_size = s ;
-	  gpuQ(hipMalloc(&p , m_seg_size )) ;
-	  m_ptrs.push_back(p) ; 
-	//  bm_ptr = self ;
-	  m_seg =0 ;
-	  m_used.push_back(0)  ;
-//std::cout<<"zzz: " <<m_seg_size<<",p= " << p<<"," <<m_ptrs[0] << std::endl ;
-	 } ; 
-	~CU_BigMem() {
-		for(int i=0 ; i<m_ptrs.size() ; i++) gpuQ(hipFree( m_ptrs[i]) ) ; 
-	}  ;
+public:
+  CU_BigMem() : m_seg_size(0), m_ptrs(0), m_used(0), m_seg(0){};
+  CU_BigMem(size_t s) { // initialize to one seg with size s
+    void *p;
+    m_seg_size = s;
+    gpuQ(hipMalloc(&p, m_seg_size));
+    m_ptrs.push_back(p);
+    //  bm_ptr = self ;
+    m_seg = 0;
+    m_used.push_back(0);
+  };
 
+  ~CU_BigMem() {
+    for (int i = 0; i < m_ptrs.size(); i++)
+      gpuQ(hipFree(m_ptrs[i]));
+  };
 
-	void * cu_bm_alloc(size_t s) {
-	  if (s  > (m_seg_size-m_used[m_seg]))  add_seg() ;
-		long * q = (long *) m_ptrs[m_seg] ;
-		int offset = m_used[m_seg]/sizeof(long) ;
-	  	void * p = (void * )   &(q[offset])  ;
-		m_used[m_seg] += ((s+sizeof(long)-1)/sizeof(long)  ) * sizeof(long)    ;
-		return p  ;
-	};
+  template <typename T> T *cu_bm_alloc(size_t s) {
+    s = s * sizeof(T);
+    
+    // pad allocations to align to 128 byte boundaries for GPU
+    constexpr size_t ALIGN = 128;
+    size_t pad = (ALIGN - s % ALIGN) % ALIGN;
+    s += pad;
+    m_pad += pad;
 
-	size_t  size() { return (m_seg+1) * m_seg_size ; } ;
-	size_t  used() { return m_seg * m_seg_size + m_used[m_seg] ; };
+    if (s > (m_seg_size - m_used[m_seg]))
+      add_seg();
+    long *q = (long *)m_ptrs[m_seg];
+    int offset = m_used[m_seg] / sizeof(long);
+    void *p = (void *)&(q[offset]);
+    m_used[m_seg] += ((s + sizeof(long) - 1) / sizeof(long)) * sizeof(long);
+    return (T *)p;
+  };
 
-	static CU_BigMem * bm_ptr  ;
-	///static void set_bm_ptr( CU_BigMem* p) {bm_ptr= p ; } ;
+  size_t size() const { return (m_seg + 1) * m_seg_size; };
+  size_t used() const { return m_seg * m_seg_size + m_used[m_seg]; };
+  size_t lost() const { return m_pad; }
 
-	//static  CU_Big_Mem* get_bm_ptr( ) { return bm_ptr ; } ;
+  static CU_BigMem *bm_ptr;
 
-private : 
-	void add_seg() { 
-		void * p ; 
-		gpuQ(hipMalloc((void**)&p , m_seg_size )) ;
-		m_ptrs.push_back(p) ;
-		m_seg++;
-		m_used.push_back(0)  ;
-	};
-		
-	std::vector<void*>  m_ptrs  ;  //points to each allocated segment 
-	int  m_seg ;  //do we need ? it's current size of mptrs -1 ;
-	size_t   m_seg_size  ; // size of each allocation
-	std::vector < size_t>  m_used ; // used memory in each allocated segment
+private:
+  void add_seg() {
+    void *p;
+    gpuQ(hipMalloc((void **)&p, m_seg_size));
+    m_ptrs.push_back(p);
+    m_seg++;
+    m_used.push_back(0);
+  };
 
-
-} ;
-
-
-
+  std::vector<void *> m_ptrs; // points to each allocated segment
+  int m_seg;                  // do we need ? it's current size of mptrs -1 ;
+  size_t m_seg_size;          // size of each allocation
+  std::vector<size_t> m_used; // used memory in each allocated segment
+  size_t m_pad{0};            // total space lost to padding
+};
 
 #endif
--- FastCaloSimAnalyzer/FastCaloGpu/src/LoadGpuFuncHist.cpp
+++ FastCaloSimAnalyzer/FastCaloGpu/src/LoadGpuFuncHist.cpp
@@ -1,154 +1,115 @@
 #include "LoadGpuFuncHist.h"
 #include "CU_BigMem.h"
 
-CU_BigMem* CU_BigMem::bm_ptr ;
+CU_BigMem *CU_BigMem::bm_ptr;
 
+LoadGpuFuncHist::~LoadGpuFuncHist() {
+  free(m_hf);
+  /*
+    hipFree((*m_hf_d).low_edge);
+    hipFree((*m_hf_d).h_szs);
+    for(unsigned int i=0 ; i< (*m_d_hf).nhist ; ++i ){
+      hipFree((*m_hf_d).h_contents[i]);
+      hipFree((*m_hf_d).h_borders[i]);
+    }
+  */
+  free(m_hf_d);
+  //  hipFree(m_d_hf) ;
 
-LoadGpuFuncHist::~LoadGpuFuncHist(){
-  free(m_hf); 
-/*
-  hipFree((*m_hf_d).low_edge);
-  hipFree((*m_hf_d).h_szs);
-  for(unsigned int i=0 ; i< (*m_d_hf).nhist ; ++i ){
-    hipFree((*m_hf_d).h_contents[i]);
-    hipFree((*m_hf_d).h_borders[i]);
-  }
-*/
-  free(m_hf_d) ;
-//  hipFree(m_d_hf) ;
-
- free(m_hf2d);
-/*
- hipFree((*m_hf2d_d).h_bordersx);
- hipFree((*m_hf2d_d).h_bordersy);
- hipFree((*m_hf2d_d).h_contents);
-*/
- free(m_hf2d_d);
-// hipFree(m_d_hf2d) ;
-
+  free(m_hf2d);
+  /*
+   hipFree((*m_hf2d_d).h_bordersx);
+   hipFree((*m_hf2d_d).h_bordersy);
+   hipFree((*m_hf2d_d).h_contents);
+  */
+  free(m_hf2d_d);
+  // hipFree(m_d_hf2d) ;
 }
-
 
 void LoadGpuFuncHist::LD2D() {
-  if(! m_hf2d ) {
-    std::cout<<"Error Load 2DFunctionHisto " << std::endl ;
-    return ;
+  if (!m_hf2d) {
+    std::cout << "Error Load 2DFunctionHisto " << std::endl;
+    return;
   }
-  
-  FH2D * hf_ptr =new FH2D ;
-  FH2D  hf = { 0, 0, 0,0, 0 };
-    
-  
-  hf.nbinsx = (*m_hf2d).nbinsx ; 
-  hf.nbinsy = (*m_hf2d).nbinsy ; 
-  //std::cout << ".....Loading  2DFnctionHists, Size of hist" <<  hf.nbinsx  << "x" << hf.nbinsy << std::endl ;  
-  //std::cout << "(*m_hf2d).h_bordersy, pointer " <<  (*m_hf2d).h_bordersy   << std::endl ;  
-/*
-  gpuQ(hipMalloc((void**)&hf.h_bordersx , (hf.nbinsx+1)*sizeof(float))) ;
-  gpuQ(hipMalloc((void**)&hf.h_bordersy , (hf.nbinsy+1)*sizeof(float))) ;
-  gpuQ(hipMalloc((void**)&hf.h_contents , (hf.nbinsy*hf.nbinsx)*sizeof(float))) ;
-*/
 
-   CU_BigMem * p = CU_BigMem::bm_ptr ;
-   
-   hf.h_bordersx = (float *) (p->cu_bm_alloc((hf.nbinsx+1)*sizeof(float) )) ;
-   hf.h_bordersy = (float *) (p->cu_bm_alloc((hf.nbinsy+1)*sizeof(float) )) ;
-   hf.h_contents  = (float *) (p->cu_bm_alloc(hf.nbinsy*hf.nbinsx*sizeof(float) )) ;
+  FH2D *hf_ptr = new FH2D;
+  FH2D hf = {0, 0, 0, 0, 0};
 
-  gpuQ(hipMemcpy( hf.h_bordersx, (*m_hf2d).h_bordersx,  (hf.nbinsx+1)*sizeof(float), 
-	hipMemcpyHostToDevice)) ;
-//  std::cout << "hf.h_bordersy, pointer " <<  hf.h_bordersy  <<  std::endl ;  
-  gpuQ(hipMemcpy( hf.h_bordersy, (*m_hf2d).h_bordersy,  (hf.nbinsy+1)*sizeof(float), 
-	hipMemcpyHostToDevice)) ;
-  gpuQ(hipMemcpy( hf.h_contents, (*m_hf2d).h_contents,  (hf.nbinsx*hf.nbinsy)*sizeof(float), 
-	hipMemcpyHostToDevice)) ;
+  hf.nbinsx = (*m_hf2d).nbinsx;
+  hf.nbinsy = (*m_hf2d).nbinsy;
 
+  CU_BigMem *p = CU_BigMem::bm_ptr;
 
-  *(hf_ptr)= hf ;
-  m_hf2d_d = hf_ptr ;
+  hf.h_bordersx = p->cu_bm_alloc<float>(hf.nbinsx + 1);
+  hf.h_bordersy = p->cu_bm_alloc<float>(hf.nbinsy + 1);
+  hf.h_contents = p->cu_bm_alloc<float>(hf.nbinsy * hf.nbinsx);
 
-//  gpuQ(hipMalloc((void**)&m_d_hf2d , sizeof(FH2D))) ;
-  m_d_hf2d = (FH2D *) (p->cu_bm_alloc(sizeof(FH2D))) ; 
-  gpuQ(hipMemcpy( m_d_hf2d, m_hf2d_d,   sizeof(FH2D), hipMemcpyHostToDevice)) ;
+  gpuQ(hipMemcpy(hf.h_bordersx, (*m_hf2d).h_bordersx,
+                 (hf.nbinsx + 1) * sizeof(float), hipMemcpyHostToDevice));
+  gpuQ(hipMemcpy(hf.h_bordersy, (*m_hf2d).h_bordersy,
+                 (hf.nbinsy + 1) * sizeof(float), hipMemcpyHostToDevice));
+  gpuQ(hipMemcpy(hf.h_contents, (*m_hf2d).h_contents,
+                 (hf.nbinsx * hf.nbinsy) * sizeof(float),
+                 hipMemcpyHostToDevice));
 
+  *(hf_ptr) = hf;
+  m_hf2d_d = hf_ptr;
 
-
-//std::cout << "LD2D: bdx,y: "<<hf.nbinsx<<"," <<  hf.nbinsy << " memeory: " <<(hf.nbinsx+hf.nbinsy+2+ hf.nbinsy*hf.nbinsx)*sizeof(float) << " M of FH2D str: "<< sizeof(FH2D)  <<std::endl ;
-
+  m_d_hf2d = p->cu_bm_alloc<FH2D>(1);
+  gpuQ(hipMemcpy(m_d_hf2d, m_hf2d_d, sizeof(FH2D), hipMemcpyHostToDevice));
 }
 
-
 void LoadGpuFuncHist::LD() {
-// this call  assume  already have Histofuncs set in m_hf 
-// this function allocate memory of GPU and deep copy m_hf to m_d_hf 
+  // this call  assume  already have Histofuncs set in m_hf
+  // this function allocate memory of GPU and deep copy m_hf to m_d_hf
 
-  if(! m_hf ) {
-    std::cout<<"Error Load WiggleHistoFunctions " << std::endl ; 
-    return ;
+  if (!m_hf) {
+    std::cout << "Error Load WiggleHistoFunctions " << std::endl;
+    return;
   }
 
-  FHs hf= {0, 0,0,0,0,0 } ;
+  FHs hf = {0, 0, 0, 0, 0, 0};
   hf.s_MaxValue = (*m_hf).s_MaxValue;
   hf.nhist = (*m_hf).nhist;
-  unsigned int * h_szs = (*m_hf).h_szs ;    // already allocateded on host ; 
-  
-   CU_BigMem * p = CU_BigMem::bm_ptr ;
-  
-// size_t  s=0 ;
+  unsigned int *h_szs = (*m_hf).h_szs; // already allocateded on host ;
 
-  
- // gpuQ(hipMalloc((void**)&hf.low_edge , (hf.nhist+1)*sizeof(float))) ;
-   hf.low_edge = (float *) (p->cu_bm_alloc((hf.nhist+1)*sizeof(float) )) ;
-  gpuQ(hipMemcpy( hf.low_edge, (*m_hf).low_edge,  (hf.nhist+1)*sizeof(float), 
-	hipMemcpyHostToDevice)) ;
-//s += (hf.nhist+1)*sizeof(float) ;
+  CU_BigMem *p = CU_BigMem::bm_ptr;
 
-//  gpuQ(hipMalloc((void**)&hf.h_szs , hf.nhist*sizeof(unsigned int))) ;
-   hf.h_szs = (unsigned int *) (p->cu_bm_alloc(hf.nhist*sizeof(float) )) ;
-  gpuQ(hipMemcpy( hf.h_szs, (*m_hf).h_szs,  hf.nhist*sizeof(unsigned int), 
-	hipMemcpyHostToDevice)) ;
-//s += hf.nhist*sizeof(unsigned int) ;
+  hf.low_edge = p->cu_bm_alloc<float>(hf.nhist + 1);
+  gpuQ(hipMemcpy(hf.low_edge, (*m_hf).low_edge, (hf.nhist + 1) * sizeof(float),
+                 hipMemcpyHostToDevice));
 
- // gpuQ(hipMalloc((void**)&hf.h_contents , hf.nhist*sizeof(uint32_t * ))) ;
- // gpuQ(hipMalloc((void**)&hf.h_borders , hf.nhist*sizeof(float * ))) ;
+  hf.h_szs = p->cu_bm_alloc<unsigned int>(hf.nhist);
+  gpuQ(hipMemcpy(hf.h_szs, (*m_hf).h_szs, hf.nhist * sizeof(unsigned int),
+                 hipMemcpyHostToDevice));
 
-   hf.h_contents = (uint32_t * *) (p->cu_bm_alloc(hf.nhist*sizeof(uint32_t *) )) ;
-   hf.h_borders = (float * *) (p->cu_bm_alloc(hf.nhist*sizeof(float *) )) ;
-//s += hf.nhist * (sizeof(uint32_t * ) +sizeof(float * )) ;
+  hf.h_contents = p->cu_bm_alloc<uint32_t *>(hf.nhist);
+  hf.h_borders = p->cu_bm_alloc<float *>(hf.nhist);
 
-  uint32_t* * contents_ptr = (uint32_t* *) malloc(hf.nhist*sizeof(uint32_t*)) ;
-  float * * borders_ptr = (float* *) malloc(hf.nhist*sizeof(float*)) ;
-  
-  for( unsigned int i =0 ; i< hf.nhist ; ++i) {
+  uint32_t **contents_ptr = (uint32_t **)malloc(hf.nhist * sizeof(uint32_t *));
+  float **borders_ptr = (float **)malloc(hf.nhist * sizeof(float *));
 
-    //  gpuQ(hipMalloc((void**)(contents_ptr+i) ,  h_szs[i]*sizeof(uint32_t))) ;
-    //  gpuQ(hipMalloc((void**)&(borders_ptr[i]) ,  (h_szs[i]+1)*sizeof(float))) ;
+  for (unsigned int i = 0; i < hf.nhist; ++i) {
 
-     contents_ptr[i] = (uint32_t *)  (p->cu_bm_alloc(h_szs[i]*sizeof(uint32_t ) )) ; 
-     borders_ptr[i] =  (float * ) (p->cu_bm_alloc((h_szs[i]+1)*sizeof(float ) )) ; 
+    contents_ptr[i] = p->cu_bm_alloc<uint32_t>(h_szs[i]);
+    borders_ptr[i] = p->cu_bm_alloc<float>(h_szs[i] + 1);
 
-     gpuQ(hipMemcpy( contents_ptr[i], (*m_hf).h_contents[i],  h_szs[i]*sizeof(uint32_t),
-	hipMemcpyHostToDevice)) ;
-      gpuQ(hipMemcpy( borders_ptr[i], (*m_hf).h_borders[i],  (h_szs[i]+1) *sizeof(float),
-	hipMemcpyHostToDevice)) ;
-  //std::cout << ".....Loading  WiggleFunctionHistss, Size of Hists[" << i << "]=" << h_szs[i]<< std::endl ;  
-//   s += h_szs[i]*sizeof(uint32_t) +( h_szs[i]+1)*sizeof(float) ;
-
+    gpuQ(hipMemcpy(contents_ptr[i], (*m_hf).h_contents[i],
+                   h_szs[i] * sizeof(uint32_t), hipMemcpyHostToDevice));
+    gpuQ(hipMemcpy(borders_ptr[i], (*m_hf).h_borders[i],
+                   (h_szs[i] + 1) * sizeof(float), hipMemcpyHostToDevice));
   }
-  
-  gpuQ(hipMemcpy( hf.h_contents, contents_ptr ,hf.nhist*sizeof(uint32_t*),hipMemcpyHostToDevice)) ;
-  gpuQ(hipMemcpy( hf.h_borders, borders_ptr ,hf.nhist*sizeof(float*),hipMemcpyHostToDevice)) ;
 
-  
- // gpuQ(hipMalloc((void**)&m_d_hf, sizeof(FHs) )) ;
-     m_d_hf = (FHs * )( p->cu_bm_alloc(sizeof(FHs ) )) ; 
-  gpuQ(hipMemcpy(m_d_hf, &hf, sizeof(FHs), hipMemcpyHostToDevice)) ;
+  gpuQ(hipMemcpy(hf.h_contents, contents_ptr, hf.nhist * sizeof(uint32_t *),
+                 hipMemcpyHostToDevice));
+  gpuQ(hipMemcpy(hf.h_borders, borders_ptr, hf.nhist * sizeof(float *),
+                 hipMemcpyHostToDevice));
 
+  m_d_hf = p->cu_bm_alloc<FHs>(1);
+  gpuQ(hipMemcpy(m_d_hf, &hf, sizeof(FHs), hipMemcpyHostToDevice));
 
-  free(contents_ptr) ;
+  free(contents_ptr);
   free(borders_ptr);
 
-  m_hf_d = &hf ;
-//std::cout << "LD1D: nhist: "<<hf.nhist<<"   memeory: " <<s << " M of FHs str: "<< sizeof(FHs)  <<std::endl ;
-
+  m_hf_d = &hf;
 }
