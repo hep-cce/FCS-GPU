c6fffc6fa4ecf7a7bc60a6af6b1f9dc4978dccde
Pengfei Ding <9438483+dingp@users.noreply.github.com>
2024-04-03 16:41:35 -0700

    parameterize image registry and project

--- scripts/build_scripts/build_images.sh
+++ scripts/build_scripts/build_images.sh
@@ -9,19 +9,20 @@ ROOT_DOT_VERSION=${ROOT_DOT_VERSION//-/.}
 
 NVHPC_BASE_IMAGE=nvcr.io/nvidia/nvhpc:${NVHPC_VERSION}
 CUDA_BASE_IMAGE=docker.io/nvidia/cuda:${CUDA_VERSION}
+REGISTRY_PROJECT=docker.io/dingpf
 
 for base_image in ${NVHPC_BASE_IMAGE} ${CUDA_BASE_IMAGE}; do
   image_tag=$(basename ${base_image})
   image_tag=${image_tag//:/} # example: nvhpc23.9-devel-cuda12.2-ubuntu22.04
 
-  root_image_tag=docker.io/dingpf/root:${ROOT_DOT_VERSION}-${image_tag} # example: dingpf/root:6.30.04-nvhpc23.9-devel-cuda12.2-ubuntu22.04
+  root_image_tag=${REGISTRY_PROJECT}/root:${ROOT_DOT_VERSION}-${image_tag} # example: dingpf/root:6.30.04-nvhpc23.9-devel-cuda12.2-ubuntu22.04
   podman-hpc build -f root.Dockerfile \
     --build-arg=BASE=${base_image} \
     --build-arg=ROOT_VERSION=${ROOT_VERSION} \
     -t ${root_image_tag} .
 
   for image_type in fcs-cuda fcs-kokkos-cuda; do
-    fcs_image_tag=docker.io/dingpf/${image_type}:${ROOT_DOT_VERSION}-${image_tag} # example: dingpf/fcs-cuda:6.30.04-nvhpc23.9-devel-cuda12.2-ubuntu22.04
+    fcs_image_tag=${REGISTRY_PROJECT}/${image_type}:${ROOT_DOT_VERSION}-${image_tag} # example: dingpf/fcs-cuda:6.30.04-nvhpc23.9-devel-cuda12.2-ubuntu22.04
     podman-hpc build -f ${image_type}.Dockerfile \
       --build-arg=BASE=${root_image_tag} \
       -t ${fcs_image_tag} .
